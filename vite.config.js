ee1ea55043fbbbcb42f97dde54709fa2ca41a5b1
Sunil Varma <sunilvarma175@gmail.com>
2025-09-25 14:53:14 -0500

    Added new image

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: '/SunilVarmaNandyala.github.io/',
+   base: "/my-beautiful-portfolio/",
 });
